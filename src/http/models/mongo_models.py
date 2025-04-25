9d6b3be4e5b15a417c1376fadd4ee052c0eb7541
Sergey Mokhov <foxleren@yandex.ru>
2025-04-25 17:03:24 +0300

    Implement api to managed MongoDB databases and users (#3)

--- src/http/models/mongo_models.py
+++ src/http/models/mongo_models.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic import BaseModel
 
 
@@ -7,3 +9,19 @@ class CreateMongoDatabaseRequestDTO(BaseModel):
 
 class DeleteMongoDatabaseRequestDTO(BaseModel):
     name: str
+
+
+class MongoPermissionDTO(BaseModel):
+    database: str
+    roles: List[str]
+
+
+class CreateMongoUserRequestDTO(BaseModel):
+    username: str
+    password_secret_name: str
+    password_secret_namespace: str
+    permissions: List[MongoPermissionDTO]
+
+
+class DeleteMongoUserRequestDTO(BaseModel):
+    username: str
